f620421d168b7508f7e582523b7df51e583a45a7
Yuliya <julgorina@yandex.ru>
2023-06-05 21:33:28 +0300

    upload SQL test task

---
+++ SQL_task.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+	"ProductName" TEXT
+);
+	
+CREATE TABLE Categories (
+    CategoryID INT PRIMARY KEY,
+	"CategoryName" TEXT
+);
+
+CREATE TABLE ProductCategories (
+    ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
+	CategoryID INT FOREIGN KEY REFERENCES Categoris(CategoryID),
+	PRIMARY KEY (ProductID, CategoryID)
+);
+
+SELECT P."ProductName", C."CategoryName"
+FROM Products P
+LEFT JOIN ProductCategories PC
+       ON p.ProductID = PC.ProductID
+LEFT JOIN Categories C
+       ON PC.CategoryID = C.CategoryID;
